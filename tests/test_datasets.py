0d85029f829cbbfea6d3f6f00e0c9012086e5146
tadasnik <tadas.nik@gmail.com>
2023-08-28 08:59:50 +0100

    generate test datasets

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,15 +1,78 @@
 import time
-import warnings
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn import cluster, datasets 
+from sklearn import cluster, datasets
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
 from split_dbscan import SplitDBSCAN
 
+
+def sklearn_datasets():
+    """Return sklearn example datasets with configuration parameters"""
+    n_samples = 500
+    random_state = 170
+    varied = datasets.make_blobs(
+        n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state
+    )
+    noisy_circles = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05)
+    noisy_moons = datasets.make_moons(n_samples=n_samples, noise=0.05)
+    blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
+    no_structure = np.random.rand(n_samples, 2), None
+    X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
+    X_aniso = np.dot(X, transformation)
+    aniso = (X_aniso, y)
+    test_datasets = [
+        (
+            noisy_circles,
+            {
+                "eps": 0.3,
+                "min_samples": 7,
+            },
+        ),
+        (
+            noisy_moons,
+            {
+                "eps": 0.3,
+                "min_samples": 7,
+            },
+        ),
+        (
+            varied,
+            {
+                "eps": 0.18,
+                "min_samples": 7,
+            },
+        ),
+        (
+            aniso,
+            {
+                "eps": 0.15,
+                "min_samples": 7,
+            },
+        ),
+        (
+            blobs,
+            {
+                "eps": 0.3,
+                "min_samples": 7
+            }
+        ),
+        (
+            no_structure,
+            {
+                "eps": 0.3,
+                "min_samples": 7
+            }
+        ),
+    ]
+    return test_datasets
+
+
+"""
 np.random.seed(0)
 
 # ============
@@ -154,4 +217,4 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
         plot_num += 1
 
 plt.show()
-
+"""
